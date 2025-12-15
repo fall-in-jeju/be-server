b6320fff32d71ac15e563bc5c29a6c3f69521c89
Parkyuchan <dbcks4848@naver.com>
2025-12-15 21:36:22 +0900

    fix: AwsLocalConfig 토큰 호출 삭제

--- src/main/java/com/jeju/ormicamp/common/config/AwsLocalConfig.java
+++ src/main/java/com/jeju/ormicamp/common/config/AwsLocalConfig.java
@@ -5,16 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider; 
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.bedrockagentruntime.BedrockAgentRuntimeAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
-// 해당 어노테이션으로 개발환경과 배포 Config 구분
-//@Profile("local")
+//@Profile("local") // 로컬 프로필 활성화 시 사용
 @RequiredArgsConstructor
 public class AwsLocalConfig {
 
@@ -22,16 +19,9 @@ public class AwsLocalConfig {
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
-
-        AwsBasicCredentials creds = AwsBasicCredentials.create(
-
-                properties.getAccessKey(),
-                properties.getSecretKey()
-        );
-
         return DynamoDbClient.builder()
                 .region(Region.of(properties.getDynamoRegion()))
-                .credentialsProvider(StaticCredentialsProvider.create(creds))
+                .credentialsProvider(DefaultCredentialsProvider.create()) 
                 .build();
     }
 
@@ -41,17 +31,4 @@ public class AwsLocalConfig {
                 .dynamoDbClient(dynamoDbClient)
                 .build();
     }
-
-    //@Bean
-    /*public BedrockAgentRuntimeAsyncClient bedrockAgentRuntimeAsyncClient() {
-        AwsBasicCredentials creds = AwsBasicCredentials.create(
-                properties.getAccessKey(),
-                properties.getSecretKey()
-        );
-
-        return BedrockAgentRuntimeAsyncClient.builder()
-                .region(Region.of(properties.getBedRockRegion()))
-                .credentialsProvider(StaticCredentialsProvider.create(creds))
-                .build();
-    }*/
 }
